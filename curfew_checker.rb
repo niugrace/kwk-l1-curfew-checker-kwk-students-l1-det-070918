80b4bf5ea48e6fc6233bbfa983fb66515f5520dd
niugrace <grace.niu.01@gmail.com>
2018-07-11 16:28:23 +0000

    automatically backed up by learn

---
+++ curfew_checker.rb
@@ -0,0 +1,25 @@
+def curfew_checker(time)
+  if time>=11
+
+def complex_curfew_checker(time)
+  if time >= 11
+    puts "you're out past curfew!"
+  else 
+    puts "you're good from punishment"
+  end
+end
+
+def curfew_checker(time)
+  if time > 11 
+    puts "you're out past curfew!"
+  elsif time == 11
+    puts "you're barely safe from punishment!"
+  else
+    puts "you're not past curfew!"
+  end
+end
+
+def 
+
+
+
